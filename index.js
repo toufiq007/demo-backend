1dba5cdd7fed27e81443ff446afdf95d94544782
toufiq007 <t.limon2342@gmail.com>
2025-06-05 10:45:08 +0600

    change the port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotevn from "dotenv";
 dotevn.config();
 const app = express();
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("this is our homepage");
